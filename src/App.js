08e2ba972cbe40e59146ea311bc390582e60bfa5
Esteban Carvajal <johan.gal.car@gmail.com>
2022-07-26 18:36:23 -0500

    Some info added nad background changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './App.css';
+import search from '../src/search.png';
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
   const [city, setCity] = useState("")
 
   const getWeather = (event) => {
-    if(event.key === "Enter") {
+    if (event.key === "Enter") {
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(
         response => response.json()
       ).then(
@@ -22,23 +23,41 @@ function App() {
 
   return (
     <div className='container'>
-      <input className='input' 
-      placeholder='Enter city...'
-      onChange={e => setCity(e.target.value)}
-      value={city}
-      onKeyPress={getWeather}
+      <div className='input-container'>
+      <input className='input'
+        placeholder='Enter city...'
+        onChange={e => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
       />
+      
+      </div>
+      {/* <button onClick={getWeather}>H
+      <img className='input-icon' src={search}/>
+      </button> */}
 
       {typeof weatherData.main === 'undefined' ? (
         <div className='message'>
           <p>Welcome to this WeatheReact App... Type a city to start!</p>
         </div>
-      ):(
-        <div className='weather-data'>
-          <p className='city'>{weatherData.name}</p>
-          <p className='celsius'>{Math.round((weatherData.main.temp) - 273.15)}ºC</p>
-          <p className='fahrenheit'>{Math.round(((9/5)*((weatherData.main.temp)-273.15))+32)}ºF</p>
-          <p className='condition'>{weatherData.weather[0].main}</p>
+
+      ) : (
+        <div>
+          <div className='weather-data'>
+            <p className='city'>{weatherData.name}</p>
+            <p className='celsius'>{Math.round((weatherData.main.temp) - 273.15)}ºC</p>
+            <p className='fahrenheit'>{Math.round(((9 / 5) * ((weatherData.main.temp) - 273.15)) + 32)}ºF</p>
+            <p className='condition'>{weatherData.weather[0].main}</p>
+          </div>
+
+          <div className='bottom'>
+            <div className='condition1'>{weatherData.main.humidity}% <p className='title1'>Humidity</p> </div>
+  
+            <div className='condition1'>{Math.round((weatherData.main.feels_like) - 273.15)}ºC <p className='title1'>Feels like</p> </div>
+            
+            <div className='condition1'>{Math.round(1.60934 * (weatherData.wind.speed))} Km/h <p className='title1'> Wind speed</p></div>
+            
+          </div>
         </div>
       )}
 
